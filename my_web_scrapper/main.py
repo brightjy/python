449b0be57453942a6c43ddcf389f13c679efa519
brightjy <barkji0@naver.com>
2020-08-31 21:42:54 +0900

    star using bs4

--- my_web_scrapper/main.py
+++ my_web_scrapper/main.py
@@ -1,6 +1,21 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 import requests
+from bs4 import BeautifulSoup
 
+thebell_result = requests.get("http://www.thebell.co.kr/free/content/Article.asp?svccode=00")
 
-thebell_result = requests.get("https://www.thebell.co.kr/free/index.asp")
+thebell_soup = BeautifulSoup(thebell_result.text, "html.parser")
 
-print(thebell_result.text)
+paging = thebell_soup.find("div", {"class":"paging"})
+
+pages = paging.find_all("a")
+
+thebell_pages = []
+
+for page in pages:
+    thebell_pages.append(page.find)
+
+print(thebell_pages[0:-1])
