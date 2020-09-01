8c1a463f203047cc12d76c08bbca5347627514b1
brightjy <barkji0@naver.com>
2020-09-01 23:17:22 +0900

    add comment

--- my_web_scrapper/main.py
+++ my_web_scrapper/main.py
@@ -1,8 +1,13 @@
+#utf-8인코딩
 import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
-import requests
+
+#링크가져오기
+import request딩
+
+#beautifulSoup(웹크롤링 용이)
 from bs4 import BeautifulSoup
 
 thebell_result = requests.get("http://www.thebell.co.kr/free/content/Article.asp?svccode=00")
