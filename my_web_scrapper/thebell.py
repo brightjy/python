d361885024023e093f158f08999c744cf902c277
brightjy <barkji0@naver.com>
2020-09-03 22:48:10 +0900

    get title

--- my_web_scrapper/thebell.py
+++ my_web_scrapper/thebell.py
@@ -5,28 +5,27 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 import requests
 from bs4 import BeautifulSoup
 
+LIMIT = "10"
 URL = "http://www.thebell.co.kr/free/content/Article.asp"
 
 def extract_thebell_pages():
     result = requests.get(URL)
-
     soup = BeautifulSoup(result.text, "html.parser")
-
     paging = soup.find("div", {"class":"paging"})
-
     pages = paging.find_all("a")
-
     thebell_pages = []
-
     for page in pages[1:-1]:
         thebell_pages.append(int(page.string))
-
     max_page = thebell_pages[-1]
     return max_page
 
 def extract_thebell_articles(last_page):
     articles = []
-    for page in range(last_page):
-        result = requests.get(f"{URL}?page={page}")
-        print(result.status_code)
+    # for page in range(last_page):
+    result = requests.get(f"{URL}?page={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser") 
+    results = soup.find_all("dl")
+    for result in results:
+        title = result.find("a")["title"]
+        print(title)
     return articles
