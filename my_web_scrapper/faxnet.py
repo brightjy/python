fce1b8de2cafb4c91caa7493c9de6b24173aa9d3
brightjy <barkji0@naver.com>
2020-09-09 21:53:35 +0900

    scrapping faxnet's headlines for studying

--- my_web_scrapper/faxnet.py
+++ my_web_scrapper/faxnet.py
@@ -5,17 +5,45 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 import requests
 from bs4 import BeautifulSoup
 
-URL_MAIN = f"https://paxnetnews.com/categories/35?"
-URL_ARTICLE = f"https://paxnetnews.com/articles/" 
+URL = f'https://paxnetnews.com'
+URL_ECONOMY = f'https://paxnetnews.com/categories/35?'
 
 def get_last_page():
-    result = requests.get(URL_MAIN)
-    soup = BeautifulSoup(result.text, "html.parser")
-    pagination = soup.find("div", {"class":"pagination"})
-    pages = pagination.find("span", {"class":"page"}).find_all("a")
+    result = requests.get(URL_ECONOMY)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    pages = soup.find('div', {'class':'pagination'}).find_all('a')
+    last_page = pages[-3].get_text(strip=True)
+    return int(last_page)
 
+def extract_article_detail(result):
+    #title
+    title = result.find('h1').find('a').text
+    #sneakpeek
+    sneakpeek = result.find("div",{"class":"desc"}).find('a').find('span').text
+    #date
+    date = result.find("div", {"class":"pubdate"}).text
+    #key
+    key = result.find('h1').find('a')['href']
+    return {
+        'title' : title,
+        'sneakpeek' : sneakpeek,
+        'date' : date,
+        'link' : f'{URL}{key}'
+    }
 
+def extract_articles(last_page):
+    articles = []
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f'{URL_ECONOMY}page={page}')
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all('div', {'class':'content'})
+        for result in results:
+            article = extract_article_detail(result)
+            articles.append(article)
+    return articles
 
 def get_articles():
     last_page = get_last_page()
-    return []
+    articles = extract_articles(last_page)
+    return articles
