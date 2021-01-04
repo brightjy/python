9012b0d391efa479cdf9ab14a1279ad39c15789f
brightjy <barkji0@naver.com>
2021-01-04 19:36:03 +0900

    flask

---
+++ SuperScrapper/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+    return render_template("potato.html")
+
+@app.route("/<username>")
+def potato(username):
+    return f"Hello {username} how are you doing"
+
+app.run()
